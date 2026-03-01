862b24a24a5eb71175dc97b58110aa86c63e2afc
CodeSleek00 <codesleekstudio@gmail.com>
2026-03-01 14:03:03 +0530

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "u298112699_lucknow_live24", "Asif1234live", "u298112699_lucknow_live24");
+$conn = mysqli_connect("152.58.131.225", "u298112699_lucknow_live24", "Asif1234live", "u298112699_lucknow_live24");
 
 if(!$conn){
     die("Database connection failed");
