d1579f19c4b5c61fd4a2618887a01192a9226d02
CodeSleek00 <codesleekstudio@gmail.com>
2026-05-28 21:22:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ while($row = mysqli_fetch_assoc($result)) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <title>News Section</title>
-<link rel="stylesheet" href="style.css?v=1.1">
+<link rel="stylesheet" href="style.css?v=1.2">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
 <!-- Font Awesome -->
