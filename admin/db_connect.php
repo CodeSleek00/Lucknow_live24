359bc92717dc03f41fb2e81dd7aea32e0c5077bb
CodeSleek00 <codesleekstudio@gmail.com>
2026-05-28 15:20:16 +0530

    c

--- admin/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("srv1259.hstgr.io", "u298112699_lucknow_live24", "Asif1234live", "u298112699_lucknow_live24");
-
-if(!$conn){
-    die("Database connection failed");
-}
-?>
