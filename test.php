ad417d7e3544de71c8b2410c7abf352227673045
CodeSleek00 <codesleekstudio@gmail.com>
2026-05-28 20:06:22 +0530

    Update test.php

--- test.php
+++ test.php
@@ -3,6 +3,10 @@ include 'database_connection/db.php';
 
 $query = "SELECT * FROM reels ORDER BY id DESC";
 $result = mysqli_query($conn, $query);
+
+if(!$result){
+    die("Query failed: " . mysqli_error($conn));
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,14 +19,20 @@ $result = mysqli_query($conn, $query);
 
 <div class="reels-container">
 
-<?php while($row = mysqli_fetch_assoc($result)) { ?>
+<?php
+if(mysqli_num_rows($result) == 0){
+    echo "<h2 style='color:white;text-align:center;margin-top:50vh;'>No reels uploaded yet</h2>";
+}
+
+while($row = mysqli_fetch_assoc($result)) {
+?>
 
 <div class="reel">
 
-    <video class="video" src="<?php echo $row['video']; ?>" muted loop></video>
+    <video class="video" src="<?php echo htmlspecialchars($row['video']); ?>" muted loop></video>
 
     <div class="overlay">
-        <h3><?php echo $row['title']; ?></h3>
+        <h3><?php echo htmlspecialchars($row['title']); ?></h3>
     </div>
 
 </div>
