e5f396476558e76b777c374ebb55138d52c7bae1
CodeSleek00 <codesleekstudio@gmail.com>
2026-05-28 19:51:56 +0530

    change

--- index.php
+++ index.php
@@ -24,6 +24,10 @@ while($row = mysqli_fetch_assoc($result)) {
 <link rel="stylesheet" href="style.css">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
+
+
 </head>
 
 <body>
